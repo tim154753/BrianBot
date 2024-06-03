38a4522e2d234319f5cdc64d67e62390ec508620
tim154753 <94345691+tim154753@users.noreply.github.com>
2024-06-02 20:40:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-import pyyoutube
+import functions
+import yt_dlp
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')[1:-1]
-GUILD = os.getenv('DISCORD_GUILD')
 intent = discord.Intents.all()
-#client = discord.Client(intents = intent)
 bot = commands.Bot(command_prefix='!', intents=intent)
+
+voice_clients = {}
+yt_dl_options = {"format": "bestaudio/best"}
+ytdl = yt_dlp.YoutubeDL(yt_dl_options)
+
+ffmpeg = {"options": '-vn'}
 
 @bot.event
 async def on_ready():
@@ -18,35 +23,23 @@ async def on_ready():
             print(f"{bot.user} is connected to the following guild:\n"f"{guild.name}(id: {guild.id})")
             members = '\n - '.join([member.name for member in guild.members])
             print(f"The following users are in {guild.name}:\n - {members}")
+
 @bot.command(name='hello')
 async def insult(ctx):
-    '''if message.author == bot.user:
-        return'''
-    #if message.content == "/hello":
-    sep = '#'
-    victim = str(ctx.author).split(sep,1)[0]
-    await ctx.channel.send(f"Hello {victim}! Why are you gay?")
+    await ctx.channel.send(f"Hello {ctx.author.name}! \nYou look lonely. I can fix that.", file=discord.File('C:/Users/Timmy/Pictures/Screenshots/beautiful2.png'))
 
 @bot.command(name='play')
 async def joinCall(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
+    if ctx.author.voice is None:
+        await ctx.send(f"You're not in a voice channel {ctx.author.name}. No trolling for you!")
+    else:
+        channel = ctx.author.voice.channel
+        await channel.connect()
 
 @bot.command(name='leave')
 async def leave(ctx):
     if ctx.voice_client is not None:
-        print("Test 1")
-        await ctx.voice_client.disconnect(force=True)
-        print("Test 2")
+        await ctx.voice_client.disconnect()
     else:
-        await ctx.send("I'm not in a voice call idiot")
-
-
-
-
-
-
-
-
-
-bot.run(TOKEN)
+        await ctx.send("I'm not in a voice channel idiot")
+    
