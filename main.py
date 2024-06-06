7f56d9e36dc9060f6e2ee442e8278461e7077864
tim154753 <94345691+tim154753@users.noreply.github.com>
2024-06-05 23:39:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-import functions
 import yt_dlp
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')[1:-1]
@@ -14,7 +14,7 @@ voice_clients = {}
 yt_dl_options = {"format": "bestaudio/best"}
 ytdl = yt_dlp.YoutubeDL(yt_dl_options)
 
-ffmpeg = {"options": '-vn'}
+ffmpeg_options = {'before_options': "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5", 'options': '-vn -filter:a "volume=0.25"'}
 
 @bot.event
 async def on_ready():
@@ -26,20 +26,59 @@ async def on_ready():
 
 @bot.command(name='hello')
 async def insult(ctx):
-    await ctx.channel.send(f"Hello {ctx.author.name}! \nYou look lonely. I can fix that.", file=discord.File('C:/Users/Timmy/Pictures/Screenshots/beautiful2.png'))
+    await ctx.channel.send(f"Hello {ctx.author.name}! , file=discord.File('path')) #put path of file you want to send as message here
+
+@bot.command(name='stop')
+async def leave(ctx):
+    if ctx.voice_client is not None:
+        try:
+            voice_clients[ctx.guild.id].stop()
+            await ctx.voice_client.disconnect()
+        except Exception as e:
+            print(e)
+    else:
+        await ctx.send("I'm not in a voice channel idiot")
 
 @bot.command(name='play')
 async def joinCall(ctx):
     if ctx.author.voice is None:
         await ctx.send(f"You're not in a voice channel {ctx.author.name}. No trolling for you!")
     else:
-        channel = ctx.author.voice.channel
-        await channel.connect()
+        try:
+            voice_client = await ctx.author.voice.channel.connect()
+            voice_clients[voice_client.guild.id] = voice_client
+        except Exception as e:
+            print(e)
+        try:
+            url = ctx.message.content.split()[1]
 
-@bot.command(name='leave')
-async def leave(ctx):
-    if ctx.voice_client is not None:
-        await ctx.voice_client.disconnect()
-    else:
-        await ctx.send("I'm not in a voice channel idiot")
-    
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download = False))
+
+            song = data['url']
+            player = discord.FFmpegOpusAudio(song, **ffmpeg_options, executable = 'C:/ffmpeg/ffmpeg.exe') # put path of ffmpeg executable in quotes
+
+            voice_clients[ctx.guild.id].play(player)
+        except Exception as e:
+            print(e)
+
+@bot.command(name='pause')
+async def pauseAudio(ctx):
+    try:
+        voice_clients[ctx.guild.id].pause()
+    except Exception as e:
+        print(e)
+@bot.command(name='resume')
+async def resumeAudio(ctx):
+    try:
+        voice_clients[ctx.guild.id].resume()
+    except Exception as e:
+        print(e)
+
+
+
+
+
+
+
+bot.run(TOKEN)
